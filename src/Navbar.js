447f32df6d4cf391749fa08fe2f99a5ba67458fa
praveensunar <140327889+praveensunar@users.noreply.github.com>
2025-02-15 19:28:10 +0530

    added banner section

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,34 +1,61 @@
-import './Navbar.css';
-import logo from './media/logo.svg';
+import React from 'react';
+import "./Navbar.css";
+// import logo from './media/logo.svg';
 export default function Navbar(){
     return(
-        <nav className="navbar">
-        <div className="part1">
-            <h3>Join our 2025 Marketing Predictions Webinar on 2/27!</h3>
-            <h4><span>WARNING:</span> Scammers are using the SmartSites brand name. Do not get scammed.<span> Read More</span> </h4>   
-        </div>
-        <div className="part2">
-            <img src={logo} alt='Logo'/>
+        // <nav classNameName="navbar">
+        // <div classNameName="part1">
+        //     <h3>Join our 2025 Marketing Predictions Webinar on 2/27!</h3>
+        //     <h4><span>WARNING:</span> Scammers are using the SmartSites brand name. Do not get scammed.<span> Read More</span> </h4>   
+        // </div>
+        // <div classNameName="part2">
+        //     <img src={logo} alt='Logo'/>
         
 
-            <ul>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Our Work</a>
-                </li>
-                <li>
-                    <a href="#">Company</a>
-                </li>
-                <li>
-                <a href="#">Contact</a>
-                </li>
-                <li>
-                <a href="tel:+2018706000" className='no'>201-870-6000</a>                   
-                </li>
-            </ul>
-        </div>
-        </nav>
-    )
+        //     <ul>
+        //         <li>
+        //             <a href="#">Services</a>
+        //         </li>
+        //         <li>
+        //             <a href="#">Our Work</a>
+        //         </li>
+        //         <li>
+        //             <a href="#">Company</a>
+        //         </li>
+        //         <li>
+        //         <a href="#">Contact</a>
+        //         </li>
+        //         <li>
+        //         <a href="tel:+2018706000" classNameName='no'>201-870-6000</a>                   
+        //         </li>
+        //     </ul>
+        // </div>
+        // </nav>
+
+<header className="site-header">
+<div  className="top-bar-header">
+<p><a href="https://www.smartsites.com/whats-in-whats-out-2025-marketing-predictions/">Join our 2025 Marketing Predictions Webinar on 2/27!</a></p>
+</div>
+
+<div className="top-bar-header">
+<p>WARNING: Scammers are using the SmartSites brand name. Do not get scammed. <a className="show-disclaimer">Read More</a></p>
+</div>
+<nav className="navbar">
+<a className="navbar-brand" href="https://www.smartsites.com/"><img fetchpriority="high" src="https://www.smartsites.com/wp-content/themes/smartsites/images/smartsites-logo.svg" alt="SmartSites Digital Marketing Agency" width="208" height="43"/></a>
+
+<div className="navbar-collapse">
+<ul className="navbar-nav">
+<li><a href="#">Services</a></li>
+<li>
+<a href="https://www.smartsites.com/work/" className="nav-link">Our Work</a></li>
+<li>
+<a href="https://www.smartsites.com/company/" className="nav-link">Company</a></li>
+<li>
+<a href="https://www.smartsites.com/contact/" className="nav-link">Contact</a></li>
+</ul>
+</div>
+<a href="tel:2018706000" className="click-call gradient-text h5">201-870-6000</a>
+</nav>
+</header>
+)
 }
--- src/servicepage.js
+++ src/servicepage.js
@@ -1,20 +1,48 @@
-import laptop from "./media/laptop.webp";
+import React from "react";
 import "./service.css";
 export default function Services(){
-    return (
-        <div>
-            <div className="banner">
-                <div className='left'>
-                <h4>Grow Your Business With Scalable Digital Marketing</h4>
-<h3>Outsmart the competition with best-in-class digital marketing services</h3>
-<h5>Get more traffic. Acquire more customers. Sell more stuff. SmartSites offers proven strategies & reliable execution to exceed your marketing goals.</h5>
-                </div>
-                <div className='right'>
-                    <img src={laptop}/>
-                </div>
-
-            </div>
-            {/* Add your service components here */}
+      return (
+ <div>
+  <section>
+    <div className="banner">
+    <div className="left-banner">
+      <div className="textcontaint">
+      <h1>Grow Your Business With Scalable Digital Marketing</h1>
+      <h2>Outsmart the competition with best-in-className digital marketing services</h2>
+      <p>Get more traffic. Acquire more customers. Sell more stuff. SmartSites offers proven strategies &amp; reliable execution to exceed your marketing goals.</p>
+      </div>
+        <div className="brandicon">
+          <ul className="list-unstyled">
+          <li>
+          <img width="60" height="51" src="https://www.smartsites.com/media/google-partner-premier-2024.svg" className="attachment-full size-full" alt="Google Partner- Premier 2024" />  </li>
+          <li>
+          <img width="144" height="50" src="https://www.smartsites.com/media/microsoft-ads-select-partner-2024.svg" className="attachment-full size-full" alt="Microsoft Advertising Select Partner 2024" />  </li>
+          <li>
+          <img width="107" height="50" src="https://www.smartsites.com/media/meta-business-partner-smartsites.svg" className="attachment-full size-full" alt="Meta Business Partner" />  </li>
+          <li>
+          <img width="206" height="79" src="https://www.smartsites.com/media/amazon-ads-verified-partner-2023.svg" className="attachment-full size-full" alt="Amazon Ads Partner"/>  </li>
+          <li>
+          <img width="118" height="50" src="https://www.smartsites.com/media/hubspot-certified-partner-1.svg" className="attachment-full size-full" alt="Hubspot Certified Partner"/>  </li>
+          <li>
+          <img width="108" height="49" src="https://www.smartsites.com/media/inc-5000-list-smartsites.svg" className="attachment-full size-full" alt="Inc. 5000 SmartSites in 8 years" />  </li>
+          <li>
+          <img width="85" height="49" src="https://www.smartsites.com/media/bbb-a-plus-rating-smartsites.svg" className="attachment-full size-full" alt="BBB A+ rating" />  </li>
+          <li>
+          <img width="117" height="48" src="https://www.smartsites.com/media/klaviyo-master-plus-champion.svg" className="attachment-full size-full" alt="Klaviyo Master Plus Champion"/></li>
+          </ul>
         </div>
-    );
-}   
+    </div>
+
+        <div className="right-banner">  
+          <img src="https://www.smartsites.com/media/digital-marketing-services-banner-hero.png" className="attachment-full size-full" alt="Digital Marketing Services banner hero"/>
+        </div>
+        </div>
+  </section>
+  <section>
+    ewdavcz hjbkjnj
+  </section>
+
+
+</div>
+      );
+    }
